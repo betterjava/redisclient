675c66d0439fa4ac4dc55f4d1e43b6d0525e5c46
serverli <serverli@163.com>
2017-05-03 20:36:44 +0800

    调整

--- src/main/java/com/cacheproxy/client/redisclient/ShardedJedisProxy.java
+++ src/main/java/com/cacheproxy/client/redisclient/ShardedJedisProxy.java
@@ -6,6 +6,7 @@ import redis.clients.jedis.JedisShardInfo;
 import redis.clients.jedis.ShardedJedis;
 
 /**
+ * @desc   分片jedis 代理工厂类
  * @author cookie
  * @email lijiaqiya@163.com
  * @date May 3, 2017
